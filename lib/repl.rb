cdd02d8f313a69b8da7593e0f8cf6c1ceb2363fe
Jonmichael Chambers <jon@tyrbo.net>
2014-06-19 09:11:24 -0600

    Implements initial REPL

--- lib/repl.rb
+++ lib/repl.rb
@@ -1,18 +1,123 @@
 require './lib/game'
+require 'dispel'
 
 class REPL
-  attr_reader :running, :playing
+  attr_reader :running, :playing, :game
 
   def initialize
     @running = false
     @playing = false
+    @game = nil
   end
 
   def run
     @running = true
 
+    print_intro
+    print_commands
+    run_loop
+    print_outro
+  end
+
+  private
+
+  def print_intro
+    puts 'Welcome to Mastermind CLI.'
+  end
+
+  def print_commands
+    if playing
+      puts 'You may (g)uess the sequence, read the (i)nstructions, reprint the list of (c)ommands, or (q)uit.'
+    else
+      puts 'You may (p)lay the game, read the (i)nstructions, reprint the list of (c)ommands, or (q)uit.'
+    end
+  end
+
+  def print_instructions
+    puts "Mastermind is a sequence guessing game."
+    puts "I will generate a sequence of four elements made up of: (R)ed, (G)reen, (B)lue, and (Y)ellow."
+    puts "The goal is to (g)uess the generated sequence in as few moves as possible."
+    puts
+  end
+
+  def run_loop
     while running
-      input = get_input
+      input, args = get_input
+      process(input, args)
+    end
+  end
+
+  def print_outro
+    puts 'Good bye.'
+  end
+
+  def get_input
+    print '> '
+    gets.strip.split
+  end
+
+  def process(command, args)
+    if    command == 'q' then quit
+    elsif command == 'c' then print_commands
+    elsif command == 'i' then print_instructions
+    elsif !playing
+      if command == 'p' then play
+      end
+    elsif playing
+      if command == 'g' then guess(args)
+      end
+    end
+  end
+
+  def play
+    @game = Game.new
+    game.start
+    @playing = true
+  end
+
+  def guess(args)
+    check_guess_hash(game.guess(args))
+  end
+
+  def check_guess_hash(result)
+    if result.is_a? Hash
+      handle_hash(result)
+    else
+      puts result
+    end
+  end
+
+  def handle_hash(result)
+    if result[:full_match]
+      handle_win(result)
+    else
+      handle_guess(result)
+    end
+  end
+
+  def handle_win(result)
+    puts "Congratulations, you guessed the sequence '#{result[:sequence]}' in #{pluralize(game.guesses.count)}."
+    quit
+  end
+
+  def handle_guess(result)
+    puts "Your guess '#{result[:guess]}' contains #{result[:matches]} correct elements in #{result[:positions]} correct positions."
+  end
+
+  def pluralize(number)
+    if number == 1
+      "#{number} guess"
+    else
+      "#{number} guesses"
+    end
+  end
+
+  def quit
+    if playing
+      puts 'Thank you for playing Mastermind.'
+      @playing = false
+    else
+      @running = false
     end
   end
 end
